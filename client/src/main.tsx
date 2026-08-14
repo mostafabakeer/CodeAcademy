cc36d84e120533c67b309549ddd506170d78e5e2
mostafabakeer <mostafabakeera@gmail.com>
2026-08-14 23:02:12 +0300

    good

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { LanguageProvider } from './i18n';
 import { AuthProvider } from './contexts/AuthContext';
 import App from './App';
@@ -8,12 +8,12 @@ import './styles/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <LanguageProvider>
         <AuthProvider>
           <App />
         </AuthProvider>
       </LanguageProvider>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
